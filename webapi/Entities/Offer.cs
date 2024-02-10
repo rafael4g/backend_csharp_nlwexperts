26db24e18f27f4afd373a4ad1ce2e889d4ab77a6
Rafael D Silva <neromad@outlook.com>
2024-02-10 16:06:06 -0300

    feat: Create Class Offer

---
+++ webapi/Entities/Offer.cs
@@ -0,0 +1,12 @@
+namespace webapi.Entities;
+
+public class Offer
+{
+    public int Id { get; set; }
+    public DateTime CreatedOn { get; set; }
+
+    public decimal Price { get; set; }
+    public int ItemId { get; set; }
+
+    public int UserId { get; set; }
+}
